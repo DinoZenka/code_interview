07019bda64116059c423d24ff7279588403b6efb
Mykhailo Hromovyi <Mykhailo.Hromovyi@spacelabs.com>
2021-10-05 21:42:01 +0300

    Fix merge conflicts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export { default as LoginPage } from './Login';
 export { default as TestListPage } from './TestList';
-=======
-export { default as LoginPage } from './Login';
->>>>>>> 3652f3f853058204e0b4921dcc9f4bbf7fba132d
